61fc9395eecf53ba10edb3da68ed6042c5860b1a
Charles Onuoha <charlesonuoha97@gmail.com>
2020-08-04 01:37:03 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import './App.css';
     render() {
         return (
             <div className= "form">
-                <input type= "text" placeholder=" Name" style={{outline:"none", position:"relative", left:"31.7%", top:"250px", height:"30px", textAlign:"center", width:"280px", boxShadow:"(0,0,1)"}}></input>
+                <input type= "text" placeholder=" Name" style={{outline:"none", position:"relative", left:"31.7%", top:"250px", height:"30px", textAlign:"center", width:"280px"}}></input>
                 <input type= "email" placeholder=" E-mail" style={{outline:"none", position:"relative",  left: "9.2%", top:"310px", height:"30px", textAlign:"center", width:"280px"}}></input>
                 <input type= "password" placeholder="Password" style={{ outline:"none", position:"relative",  left: "-13.2%", top:"370px", height:"30px", textAlign:"center", width:"280px"}}></input>
                 <button style={{outline:"none", backgroundColor:"teal", color:"white", position:"relative", width:"300px", height:"30px", top:"430px", right:"36%"}}> Go! </button>
