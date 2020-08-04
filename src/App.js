179c4ed9fe60b029f39d2fdda91ff76e7b835134
Charles Onuoha <charlesonuoha97@gmail.com>
2020-08-04 01:36:17 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Thoughts from './Home.js';
+import Head from './head.js';
+
+function App() {
+  return (
+    <div className="App">
+     <Thoughts />
+     <Head />
+    </div>
+
+    
+  );
+}
+
+export default App;
---
+++ src/head.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Head extends Component{
+    render() {
+        return(
+            <div id="sign">
+                <h3 style={{position:"relative", color: "black", textAlign: "center", left:"-2%", top: "90px"}}>SIGN UP</h3> 
+            </div>
+        )
+    }
+};
+
+export default Head;
